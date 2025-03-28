6003b91e5537fb056b6b8216872b505a71f5257b
Abhinav <abhinav01.galaxy@gmail.com>
2025-03-28 11:05:25 +0530

    initial

---
+++ c3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "hey" << endl;
+    cout << "hey hi2" << endl; //"added new line"
+    cout << "hey hi3" << endl; //"added another new line"
+
+    return 0;
+}
